8224752d5cddff32568e4d33c23c5baf517228b6
sergio-0103 <sergio_0103@list.ru>
2024-08-01 13:44:37 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list=['apple', 'frukt', 'frukt1', 'frukt2','frukt3']
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2]='juce'
+print(my_list)
+
+my_dict={'один':'one','два':'two','три':'three'}
+print(my_dict)
+print(my_dict['один'])
+print(my_dict.get('два'))
+my_dict['четыре']='four'
+print(my_dict)
+
